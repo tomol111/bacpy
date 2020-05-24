fb963c42039f8d6396728f44e55d572feed51389
Tomasz Olszewski <tomol111@wp.pl>
2020-05-24 18:07:15 +0200

    Giving up with make game supporting multiple iterface types and marging files into one

--- bacpy.py
+++ bacpy.py
@@ -1,17 +1,55 @@
-"""This file defines CLI for game"""
+"""BacPy - Bulls and Cows game implementation by Tomasz Olszewski"""
 
-import re
+
+__version__ = '0.2'
+__author__ = 'Tomasz Olszewski'
+
+
+from collections import Counter
+from dataclasses import dataclass
 import subprocess
 import sys
+import random
+import re
 
-from core import AbstractGame, AbstractRound, \
-        QuitGame, RestartGame, CancelOperation
+
+# ========
+# Events
+# ========
+
+
+class GameEvent(Exception):
+    """Base game event class."""
+
+
+class QuitGame(GameEvent):
+    """Quit game event."""
+
+
+class RestartGame(GameEvent):
+    """Restart game event."""
+
+
+class CancelOperation(GameEvent):
+    """Operation canceled event."""
+
+
+# ============
+# Core classes
+# ============
+
+
+@dataclass(frozen=True)
+class Difficulty:
+    """Game difficulty parameters."""
+    name: str
+    digs_set: frozenset
+    digs_range: str
+    num_size: int
 
 
 class CLITools:
-    """Basic class for CLI."""
-
-    io_type = 'CLI'
+    """Class with basic methods for CLI."""
 
     def _ask_ok(self, prompt, default=True):
         """Yes-No input."""
@@ -43,8 +81,88 @@ class CLITools:
             pager.wait()
 
 
-class Round(AbstractRound, CLITools):
-    """Completed round class with CLI."""
+class Round(CLITools):
+    """Round class."""
+
+    def __init__(self, difficulty):
+        self.set_difficulty(difficulty)
+        self._draw_number()
+        print(self._number) # TESTING PRINT
+        self._steps = None
+
+    def set_difficulty(self, difficulty):
+        """Setting difficulty."""
+        self._difficulty = difficulty.name
+        self._num_size = difficulty.num_size
+        self._digs_set = difficulty.digs_set
+        self._digs_range = difficulty.digs_range
+
+    def _draw_number(self):
+        """Draw number digits from self._digs_set."""
+        self._number = ''.join(
+            random.sample(self._digs_set, self._num_size)
+        )
+
+    def _loop(self):
+        """Round main loop."""
+        self._steps = 1
+        while True:
+            bulls, cows = self._comput_bullscows(self._number_input())
+
+            if bulls == self._num_size:
+                self._score_output()
+                return
+
+            self._bulls_and_cows_output(bulls, cows)
+            self._steps += 1
+
+    def run(self):
+        self._start_round()
+        try:
+            self._loop()
+        finally:
+            self._end_round()
+
+    def _comput_bullscows(self, guess):
+        """Return bulls and cows for given input."""
+        bulls, cows = 0, 0
+
+        for i in range(self._num_size):
+            if guess[i] == self._number[i]:
+                bulls += 1
+            elif re.search(guess[i], self._number):
+                cows += 1
+
+        return bulls, cows
+
+    def is_number_valid(self, number, outputs=True):
+        """Check if given number string is valid."""
+
+        is_correct = True
+
+        # Check if number have wrong characters
+        wrong_chars = set(number) - self._digs_set
+        if wrong_chars:
+            is_correct = False
+            if outputs:
+                self._wrong_chars_in_num_output(wrong_chars, number)
+
+        # Check length
+        if len(number) != self._num_size:
+            is_correct = False
+            if outputs:
+                self._wrong_num_len_output(len(number))
+
+        # Check that digits don't repeat
+        digits = Counter(number)
+        rep_digs = {i for i, n in digits.items() if n > 1}
+        rep_digs -= wrong_chars
+        if rep_digs:
+            is_correct = False
+            if outputs:
+                self._rep_digs_in_num_output(rep_digs, number)
+
+        return is_correct
 
     def _start_round(self):
         """Print round starting message"""
@@ -62,9 +180,10 @@ class Round(AbstractRound, CLITools):
         """Print round ending message"""
         print('======= Round ended ======\n')
 
+
     def _wrong_chars_in_num_output(self, wrong_chars, wrong_input):
         """Print wrong characters in number message"""
-        wrong_chars = ', '.join(map(lambda x: "'"+x+"'", wrong_chars))
+        wrong_chars = ', '.join(map(lambda x: f"'{x}'", wrong_chars))
         print(
             f'  Wrong characters: {wrong_chars}.'
             f' Correct are: "{self._digs_range}".'
@@ -77,7 +196,7 @@ class Round(AbstractRound, CLITools):
     def _rep_digs_in_num_output(self, rep_digs, wrong_input):
         """Print repeated digits in number message"""
         rep_digs_str = ', '.join(
-            map(lambda x: "'"+x+"'", rep_digs)
+            map(lambda x: f"'{x}'", rep_digs)
         )
         print(f"  Number can't have repeated digits. {rep_digs_str} repeated.")
 
@@ -130,6 +249,14 @@ class Round(AbstractRound, CLITools):
 
             return input_
 
+    def _bulls_and_cows_output(self, bulls, cows):
+        """Print bulls and cows message"""
+        print(f"  bulls: {bulls:>2}, cows: {cows:>2}")
+
+    def _score_output(self):
+        """Print score message"""
+        print(f"\n *** You guessed in {self._steps} steps ***\n")
+
     def special_input_hint_output(self):
         """Print hint for special inputs message"""
         print('\n'.join([
@@ -155,21 +282,69 @@ class Round(AbstractRound, CLITools):
             args=['-C'] # Prevent from showing text on bottom of the screen
         )
 
-    def _score_output(self):
-        """Print score message"""
-        print(f"\n *** You guessed in {self._steps} steps ***\n")
 
+class Game(CLITools):
+    """Game class."""
 
-    def _bulls_and_cows_output(self, bulls, cows):
-        """Print bulls and cows message"""
-        print(f"  bulls: {bulls:>2}, cows: {cows:>2}")
+    DIFFICULTIES = [Difficulty(*record) for record in (
+        ('easy',   frozenset('123456'),         '1-6',     3),
+        ('normal', frozenset('123456789'),      '1-9',     4),
+        ('hard',   frozenset('123456789abcdf'), '1-9,a-f', 5),
+    )]
 
+    def __init__(self, difficulty=None):
+        if difficulty is not None:
+            self.set_difficulty(difficulty)
+        else:
+            self._difficulty = None
 
-class Game(AbstractGame, CLITools):
-    """Completed game class with CLI."""
+    def set_difficulty(self, difficulty=None):
+        """Setting game difficulty.
 
-    def __init__(self, Round=Round, difficulty=None):
-        super().__init__(Round, difficulty)
+        Ask user if not given directly.
+        Prevent from set None when self.difficulty_input()
+        raise CancelOperation.
+        """
+        # Ask for difficulty if not given directly
+        if difficulty is None:
+            difficulty = self.difficulty_input()
+
+        self._difficulty = difficulty
+
+    def _loop(self):
+        """Main Game loop."""
+        try:
+            self.set_difficulty()
+        except CancelOperation:
+            return
+
+        while True: # Game loop
+            try:
+                Round(self._difficulty).run()
+            except RestartGame:
+                continue
+            except QuitGame:
+                return
+
+            try:
+                if self._ask_if_continue_playing():
+                    continue
+                else:
+                    return
+            except CancelOperation:
+                return
+
+    def run(self):
+        """Runs game loop.
+
+        It is responsible to call self._start_game and self._end_game.
+        """
+
+        self._start_game()
+        try:
+            self._loop()
+        finally:
+            self._end_game()
 
     def difficulty_input(self):
         """Printing options table and taking from user difficulty option."""
@@ -238,3 +413,7 @@ class Game(AbstractGame, CLITools):
     def _ask_if_continue_playing(self):
         """Ask user if he want to continue playing"""
         return self._ask_ok('Do you want to continue? [Y/n]: ')
+
+
+if __name__ == '__main__':
+    Game().run()
